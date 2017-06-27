9430e476f74359619495d66424ed9bae2ab5bb4c
Jee Hoon <yamoo9@naver.com>
2017-06-27 10:36:29 +0900

    노드 복제 실습 자료 추가

--- LECTURE/DAY16/study/dom.script.js
+++ LECTURE/DAY16/study/dom.script.js
@@ -2,5 +2,51 @@
 (function(global, document, $){
   'use strict';
 
+  // ——————————————————————————————————————
+  // Node Interface
+  // ——————————————————————————————————————
+
+  // 노드 복제
+  // .cloneNode([deep])
+
+  // 클래스 속성 제어
+  // .classList
+
+  // 데이터 접두사 속성 제어
+  // .dataset
+
+  // 속성 제어
+  // .getAttribute()
+  // .setAttribute()
+  // .removeAttribute()
+  // .hasAttribute()
+
+  // DOM 삽입
+  // .insertAdjacentHTML()
+  // .insertAdjacentElement()
+  // .insertAdjacentText()
+
+  // 스타일 제어
+  // .style
+  // .cssText
+  // .getComputedStyle | currentStyle
+
+  // ——————————————————————————————————————
+  // Event Interface
+  // ——————————————————————————————————————
+
+  // .on{event_type}
+  // .addEventListener() | .attachEvent()
+  // .removeEventListener() | .detachEvent()
+
+  // 이벤트 객체
+  // 이벤트 타켓
+  // 이벤트 기본 동작 차단
+  // 이벤트 전파 흐름
+  // 이벤트 위임
+  // 이벤트 커런트 타겟
+  // 이벤트 전파 차단
+  // 이벤트 전파 즉시 차단
+
 
 })(window, window.document, window.FDS);
