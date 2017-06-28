0f5e3f1d6c57c4a73292ce8de4602cb3d8d5132e
Jee Hoon <yamoo9@naver.com>
2017-06-28 18:00:22 +0900

    객체지향 자바스크립트 패턴

--- LECTURE/DAY17/study/dom.script.js
+++ LECTURE/DAY17/study/dom.script.js
@@ -26,7 +26,7 @@
     // SET
     $.attr(li, 'style', 'color: tan');
     // GET
-    console.log( $.attr(li, 'style') );
+    // console.log( $.attr(li, 'style') );
     // Event
     li.onclick = function() {
       $.removeAttr(this, 'style');
@@ -60,21 +60,30 @@
   zoom_button_set.onclick = function(e) {
     // console.log('this: %s, e.taget: %s, e.currentTarget: %s', this, e.target, e.currentTarget);
     var button = e.target;
+    // 누구의 글자 크기를 키울 것인가?
+    // var current_fontsize = $.attr(list, 'style');
+    // console.log(current_fontsize);
+    // 계산된 속성 값을 가져와야 한다.
+    var current_val = getComputedStyle(list).fontSize;
+    current_val = global.parseInt(current_val, 10);
+
     if ( button.classList.contains('zoom-in') ) {
       // console.log('zoom in');
-      // 누구의 글자 크기를 키울 것인가?
-      // var current_fontsize = $.attr(list, 'style');
-      // console.log(current_fontsize);
-      // 계산된 속성 값을 가져와야 한다.
-      var current_val = getComputedStyle(list).fontSize;
-      current_val = global.parseInt(current_val, 10);
-      var new_val = current_val + 3;
+      var new_val = current_val + 10;
       $.attr(list, 'style', 'font-size: ' + new_val + 'px');
       // $.css(list, 'font-size', new_val + 'px');
+      // $.css(list, '\
+      //   font-size: 200px;\
+      //   padding: 100px;\
+      //   margin: 20px;\
+      //   border: 20px solid currentColor;\
+      // ');
     }
     if ( button.classList.contains('zoom-out') ) {
       // console.log('zoom out');
       // 누구의 글자 크기를 줄일 것인가?
+      var new_val = current_val - 10;
+      $.attr(list, 'style', 'font-size: ' + new_val + 'px');
     }
   };
 
@@ -86,10 +95,87 @@
   // .addEventListener() | .attachEvent()
   // .removeEventListener() | .detachEvent()
 
+  // ---------------------------------------
+  // Cross Browsing Event Utility Function
+  // ---------------------------------------
+  // W3C 표준 모델 : addEventListener
+  // MS 비표준 모델 : attachEvent
+  // 구형 이벤트 모델 : on{type}
+  // function addEvent(el, type, handler, capture) {
+  //   if ( el.addEventListener ) {
+  //     capture = capture || false;
+  //     el.addEventListener(type, handler, capture);
+  //   } else if ( el.attachEvent ) {
+  //     el.attachEvent('on'+type, handler);
+  //   } else {
+  //     el['on'+type] = handler;
+  //   }
+  // }
+
+  // 함수 값이 아닌, 참조 변수를 설정해야만 제거가 된다.
+  global.addEvent = function(){
+    var _addEvent = null;
+    if ( 'addEventListener' in EventTarget.prototype ) {
+      _addEvent = function(el, type, handler, capture) {
+        el.addEventListener(type, handler, capture || false);
+      };
+    } else if ( 'attachEvent' in EventTarget.prototype ) {
+      _addEvent = function(el, type, handler, capture) {
+        el.attachEvent('on'+type, handler);
+      };
+    } else {
+      _addEvent = function(el, type, handler, capture) {
+        el['on'+type] = handler;
+      };
+    }
+    return _addEvent;
+  }();
+  global.removeEvent = function(){
+    var _removeEvent = null;
+    if ( 'removeEventListener' in EventTarget.prototype ) {
+      _removeEvent = function(el, type, handler, capture) {
+        el.removeEventListener(type, handler, capture || false);
+      };
+    } else if ( 'attachEvent' in EventTarget.prototype ) {
+      _removeEvent = function(el, type, handler, capture) {
+        el.detachEvent('on'+type, handler);
+      };
+    } else {
+      _removeEvent = null;
+    }
+    return _removeEvent;
+  }();
+
+
   // 이벤트 객체
   // 이벤트 타켓
+
+  // document.onclick = function(e) {
+  //   e = e || window.event;
+  //   e.target = e.target || e.srcElement;
+  //   e.relatedTarget = e.type === 'mousever' ? e.fromElement : e.toElement;
+  // };
+
+  var actionA = function() {
+    console.log(this, 'actionA');
+  };
+  var actionB = function() {
+    console.log(this, 'actionB');
+  };
+
+  addEvent(list, 'click', actionA);
+  addEvent(list, 'click', actionB);
+  addEvent(list, 'click', function(){
+    removeEvent(this, 'click', actionB);
+  });
+
   // 이벤트 기본 동작 차단
+  // e.preventDefault()   VS   return false
   // 이벤트 전파 흐름
+  // capturing  vs  bubbling
+  // 전파 차단
+  // e.stopPropagation()
+  // e.stopImmediatePropagation()
   // 이벤트 위임
   // 이벤트 커런트 타겟
 
