a8fd99fb9204d6e14842cfc1b9706857b2b3db9f
Jee Hoon <yamoo9@naver.com>
2017-07-11 13:55:03 +0900

    jQuery Scroll Event Control

--- LECTURE/DAY21/lecture/js/main.js
+++ LECTURE/DAY21/lecture/js/main.js
@@ -37,7 +37,53 @@
   'use strict';
 
   // 메서드 체이닝이 가능한 이유는
-  // jQuery 객체가 메서드(모든 메서드가 가능한 것은 아님)마다 반환되기 때문
+  // jQuery 객체가 메서드마다 반환되기 때문
+  // > 모든 메서드가 가능한 것은 아님 (API 문서 확인)
 
-  $('h1').css('color', 'tan').addClass('is-3').removeClass('is-1');
+  // 팩토리 함수
+  // CSS 선택자를 전달
+
+  var _this = this;
+
+  $('h1')
+  // CSS 속성을 제어하는 jQuery 객체(인스턴스)의 메서드를 사용
+  .css('color', 'tan').addClass('is-3').removeClass('is-1');
+
+  // jQuery 인스턴스 메서드 검증
+  console.groupCollapsed('jQuery 인스턴스 메서드 검증');
+
+  console.log('jQuery.prototype.css', !!jQuery.prototype.css);
+  console.log('jQuery.prototype.addClass', !!jQuery.prototype.addClass);
+  console.log('jQuery.prototype.removeClass', !!jQuery.prototype.removeClass);
+  console.log('jQuery.prototype.radioClass', !!jQuery.prototype.radioClass);
+
+  console.groupEnd('jQuery 인스턴스 메서드 검증');
+
+  // DOM Node 전달
+  // DocumentNode
+  // Host Object 전달
+  // Document {}
+  // .on() jQuery 인스턴스 메서드
+  // $(document).on('click', function(e) {
+  $(document).on('click', function (e) {
+    console.log(e.target); // Event Object 의 target 속성 값은?
+    console.log(e.currentTarget); // Event Object 의 currentTarget 속성 값은?
+    // 중요!!!!!
+    // 함수 값(리터럴)에서는 this가 이벤트의 주인을 가리키지만,
+    // 화살표 함수 내 this는 상위 컨텍스트를 가리킨다.
+    console.log(_this); // Arrow Function 내부의 this는?
+  });
+
+  // Window {}
+  // 사용자가 스크롤 이벤트를 발생시키면,
+  // 콜백 함수가 실행된다.
+  var $window = $(window);
+  var $main = $('.main');
+
+  $window.on('scroll', function () {
+    $window.scrollTop() > 123 ?
+    // this.scrollTop() > 123 ?
+    $main.addClass('is-fixed') : $main.removeClass('is-fixed');
+  });
+  // }.bind($window));
 })(window, window.jQuery);
