013f29677d0d818099feb2308800e0fb1a0d30d6
Jee Hoon <yamoo9@naver.com>
2017-07-19 16:11:12 +0900

    Vue + DOM 스크립트 기반 이벤트 핸들링 + 라이프 사이클 훅

--- LECTURE/DAY25/vue.js-study/src/es6/conditional.js
+++ LECTURE/DAY25/vue.js-study/src/es6/conditional.js
@@ -1,53 +1,80 @@
 /*! main.js @ 2017, yamoo9.net */
-((global)=>{
+((global, $)=>{
   'use strict';
 
   // 마운트 엘리먼트 설정
   // 데이터 설정
   // true or false 값을 가진 데이터 속성을 추가
 
+  let data = {
+    is_login: false,
+    numbers: 10,
+    users: [
+      {
+        name: '김민기',
+        img: 'https://search.pstatic.net/common?type=o&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201608%2F20160816113515714-5897352.jpg',
+        id: '@mk.kim',
+        bio: 'iure nulla distinctio vero, non itaque pariatur unde exercitationem enim alias labore eligendi.'
+      },
+      {
+        name: '김진아',
+        img: 'https://search.pstatic.net/common?type=o&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F53%2F201007211101455201.jpg',
+        id: '@hm.song',
+        bio: 'Voluptates doloremque illum error, odit quibusdam maiores sint, consequatur.'
+      },
+      {
+        name: '유건',
+        img: 'https://search.pstatic.net/common?type=o&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F149%2F201107201212419221.jpg',
+        id: '@sy.choun',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+      }
+    ],
+    metopo: {
+      concept: '막춤',
+      who: '막장 드라마 주인공'
+    }
+  };
+
+  let methods = {
+    changeMode() {
+      this.is_login = !this.is_login;
+    }
+  };
+
   let bootstrap = {
     el: '#app',
-    data: {
-      is_login: false,
-      numbers: 10,
-      users: [
-        {
-          name: '김민기',
-          img: 'https://search.pstatic.net/common?type=o&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201608%2F20160816113515714-5897352.jpg',
-          id: '@mk.kim',
-          bio: 'iure nulla distinctio vero, non itaque pariatur unde exercitationem enim alias labore eligendi.'
-        },
-        {
-          name: '김진아',
-          img: 'https://search.pstatic.net/common?type=o&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F53%2F201007211101455201.jpg',
-          id: '@hm.song',
-          bio: 'Voluptates doloremque illum error, odit quibusdam maiores sint, consequatur.'
-        },
-        {
-          name: '유건',
-          img: 'https://search.pstatic.net/common?type=o&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F149%2F201107201212419221.jpg',
-          id: '@sy.choun',
-          bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-        }
-      ],
-      metopo: {
-        concept: '막춤',
-        who: '막장 드라마 주인공'
-      },
-
-    },
-    methods: {
-      changeMode() {
-        this.is_login = !this.is_login;
-      }
+    data,
+    methods,
+    // Life Cycle Hook
+    // created () {
+    // console.log('Created');
+    // },
+    // beforeMount () {
+    //   console.log('before Mount');
+    //   console.log(this.users);
+    // },
+    mounted () {
+      console.log('Mounted');
+      $('.add-content-btn').on('click', function(){
+        console.log('click');
+        var $prev = $(this).prev();
+        console.log($prev.length);
+        var o = global.app.metopo;
+        var key = 'user_input';
+        var value = $prev.val();
+        // Vue 생성자 함수의 메서드인 set()을 사용하는 방법
+        Vue.set(o, key, value);
+        // Vue{} 인스턴스(객체) 메서드를 사용하는 방법
+        // Vue{} 등록된 methods 안의 this.$set()
+        $prev.val('');
+      });
     }
   };
 
   // 뷰 객체 생성
   global.app = new Vue(bootstrap);
 
-})(window);
+})(window, window.jQuery);
 
 (function(global, $, Vue){
   'use strict';
