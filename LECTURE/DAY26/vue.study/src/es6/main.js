54e59cce8abc159e700effba223302d70cbf761a
Jee Hoon <yamoo9@naver.com>
2017-07-21 18:01:53 +0900

    Vue 컴퓨티드 프로퍼티란?

--- LECTURE/DAY26/vue.study/src/es6/main.js
+++ LECTURE/DAY26/vue.study/src/es6/main.js
@@ -12,13 +12,17 @@
   let firebase_db = 'https://vue-planlist.firebaseio.com/planlist.json';
 
   new Vue({
+
     el: '.project-team',
+
     created () {
       // Ajax 통신을 통해 Sever(Firebase)로부터 데이터를 GET 한 후,
       // Vue 인스턴스 데이터에 GET 한 콘텐츠를 할당(Assign)
-      this.getFirebasePlanData();
+      // this.getFirebasePlanData();
     },
+
     data: {
+
       team: {
         name: 'FDS 4기 프로젝트 명단',
         group: [
@@ -33,13 +37,68 @@
           ['허우창', '정미주', '기미송']
         ]
       },
+
       is_transfer: false,
       is_blank: true,
       plan_it: '',
       plan: [],
-      plan_keys: []
+      plan_keys: [],
+
+      links: [
+        {
+          href: 'https://google.com',
+          // target: '_blank',
+          external: true,
+          content: 'Google'
+        },
+        {
+          href: 'https://adobe.com',
+          // target: false,
+          external: true,
+          content: 'Adobe'
+        },
+        {
+          href: 'https://daum.net',
+          // target: false,
+          external: false,
+          content: 'DAUM'
+        }
+      ],
+
+      numbers: '1902 12 32 11 45 90 456 129 19 8 12'.split(' '),
+
+      todos: [
+        {finished: false, what: '도서 읽기'},
+        {finished: true, what: '이웃 돕기'},
+        {finished: false, what: '학원 등록'},
+        {finished: true, what: '영어 스터디 모임'},
+        {finished: false, what: '수제 도식락 만들기'},
+        {finished: true, what: '일본 여행 예약'},
+      ]
+
     },
+
+    computed: {
+      finished_todos(){
+        return this.todos.filter(doit=>doit.finished);
+      },
+      oddNumbers(){
+        return this.numbers.filter(n=>n%2!==0);
+      },
+      reverse_team_name(){
+        return this.team.name.split(' ').reverse().join(' / ');
+      }
+    },
+
     methods: {
+      toggleClass(name, e){
+        let el = e.target;
+        if ( el.classList.contains(name) ) {
+          el.classList.remove(name);
+        } else {
+          el.classList.add(name);
+        }
+      },
       getFirebasePlanData(){
         $.get(firebase_db).then(data=>{
           this.plan_keys = Object.keys(data);
@@ -76,6 +135,7 @@
         this.postFirebasePlanData(new_plan_it);
       }
     }
+
   });
 
 })(window);
