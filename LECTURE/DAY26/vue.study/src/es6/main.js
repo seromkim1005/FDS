d34141b62dd2cdd5cc119bcbc8987b5662c4490f
Jee Hoon <yamoo9@naver.com>
2017-07-21 15:44:26 +0900

    vue 속성 바인딩

--- LECTURE/DAY26/vue.study/src/es6/main.js
+++ LECTURE/DAY26/vue.study/src/es6/main.js
@@ -8,8 +8,16 @@
     f2: 113
   };
 
+  // Firebase DB 통신 주소
+  let firebase_db = 'https://vue-planlist.firebaseio.com/planlist.json';
+
   new Vue({
     el: '.project-team',
+    created () {
+      // Ajax 통신을 통해 Sever(Firebase)로부터 데이터를 GET 한 후,
+      // Vue 인스턴스 데이터에 GET 한 콘텐츠를 할당(Assign)
+      this.getFirebasePlanData();
+    },
     data: {
       team: {
         name: 'FDS 4기 프로젝트 명단',
@@ -25,14 +33,21 @@
           ['허우창', '정미주', '기미송']
         ]
       },
+      is_transfer: false,
       plan_it: '',
-      plan: [
-        { done: false, it: '아이디어 브레인 스토밍' }
-      ]
+      plan: []
     },
     methods: {
+      getFirebasePlanData(){
+        $.get(firebase_db).then(data=>{
+          this.plan = Object.values(data);
+        });
+      },
       excludeMember(i,n){
         this.team.group[i].splice(n, 1);
+      },
+      updatePlanIt(e){
+        this.plan_it = e.target.value;
       },
       addPlanIt(){
         let _this = this;
@@ -40,7 +55,17 @@
           it: _this.plan_it,
           done: false
         };
-        _this.plan.push(new_plan_it);
+        // _this.plan.push(new_plan_it);
+        this.is_transfer = true;
+        this.new_plan_it = '';
+
+        // Firebase DB 통신으로 새로운 플랜 추가하기
+        $.post(firebase_db, JSON.stringify(new_plan_it)).then(data => {
+          // POST 통신이 성공하게 되면,
+          // 다시 GET 통신
+          this.getFirebasePlanData();
+          this.is_transfer = false;
+        });
       }
     }
   });
