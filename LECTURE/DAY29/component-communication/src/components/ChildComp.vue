e7f2b9a337690544453ab0c87489ec66e5050c10
Jee Hoon <yamoo9@naver.com>
2017-07-26 12:11:58 +0900

    부모 자식 간 통신

--- LECTURE/DAY29/component-communication/src/components/ChildComp.vue
+++ LECTURE/DAY29/component-communication/src/components/ChildComp.vue
@@ -5,28 +5,42 @@
     ul
       li {{ parentName }}
       li {{ temp }}
+      li Computed: {{ mine_propMessage }}
+      //- li Data: {{ mine_prop_msg }}
+    button(
+      type="button"
+      @click="mine_prop_msg = 'change data from child component'")
+      | Chanage Prop Message
 </template>
 
 <script>
 export default {
   // 상위 컴포넌트로부터 전달된 속성을 받기 위해서는
   // props 를 통해 명시적 선언을 해야 한다.
-  props: ['parentName', 'temp'],
+  props: ['parentName', 'temp', 'prop-message'],
   data () {
     return {
-      mine: 'Child Component'
+      mine: 'Child Component',
+      mine_prop_msg: this.propMessage,
+      copyed_message: []
+    }
+  },
+  computed: {
+    mine_propMessage() {
+      this.copyed_message = this.propMessage.slice();
+      return this.copyed_message.join(' ');
     }
   }
 }
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
 .component
   display: flex
   flex-direction: column
   justify-content: center
-  align-items: center
-  margin: 1em
+  align-items: flex-start
+  margin-top: 1em
   padding: 1.4em
   border: 3px solid currentColor
   color: #957f65
@@ -34,6 +48,7 @@ export default {
     font-weight: 100
 
   ul
+    list-style: none
     padding-left: 0
 </style>
 
--- LECTURE/DAY29/component-communication/src/components/ParentComp.vue
+++ LECTURE/DAY29/component-communication/src/components/ParentComp.vue
@@ -1,10 +1,23 @@
 <template lang="pug">
   div.component
     h1 {{ mine }}
+    label
+      input(type="checkbox" v-model="message" value="오늘도")
+      | 오늘도
+    label
+      input(type="checkbox" v-model="message" value="내일도")
+      | 내일도
+    label
+      input(type="checkbox" v-model="message" value="행복하길")
+      | 행복하길
+    label
+      input(type="checkbox" v-model="message" value="불행하길")
+      | 불행하길
     child(
       class="child-component"
       parent-name="ParentComponent"
-      temp="1990"
+      :temp="1990"
+      :prop-message="message"
     )
 </template>
 
@@ -16,6 +29,12 @@ export default {
   },
   data () {
     return {
+      // 부모에서 전달된 속성 유형이 참조형이라면
+      // 객체, 배열이라면... 자식은 그 데이터를 그대로 사용하여
+      // 수정하면 안된다.
+      // 방법 1. 복사해서 사용해야 한다
+      // 방법 2. 부모에게 수정 요청을 한다. (이벤트 발신)
+      message: [],
       mine: 'Parent Component'
     }
   }
@@ -25,8 +44,9 @@ export default {
 <style lang="sass">
 .component
   display: flex
+  flex-direction: column
   justify-content: center
-  align-items: center
+  align-items: flex-start
   margin: 1em
   padding: 1.4em
   border: 3px solid currentColor
